bf23a42fb7888015d0e393dceca7814ab30ba1dd
Zack Arnett <arnett.zackary@gmail.com>
2019-06-23 00:06:48 -0400

    Test

--- src/socket.ts
+++ src/socket.ts
@@ -47,7 +47,7 @@ export function createSocket(server: Server) {
       updateGameState(io, currentGame);
     });
 
-    socket.on("placeBet", (gameID: string) => {
+    socket.on("tet", (gameID: string) => {
       const currentGame = getGame(games, gameID);
 
       nextRound(currentGame);
